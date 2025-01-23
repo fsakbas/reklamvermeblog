412b9cc88e9b3bcc04222282d5fe41afe79fab6b
fsakbas <fsakbas@212medya.com>
2025-01-23 10:09:47 +0300

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'destek@reklamverme.com.tr',
   link: 'https://blog.reklamverme.com.tr',
   description: 'Dijital Pazarlama Blogu',
-  lang: 'tr-TR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Istanbul', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
